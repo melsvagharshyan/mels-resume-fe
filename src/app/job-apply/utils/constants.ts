b5adba68b002bac7e79ebe97f4767bebc317e8fa
Arsen <arsen.simonian@haqqex.com>
2025-09-27 14:31:28 +0400

    current commit

--- src/app/job-apply/utils/constants.ts
+++ src/app/job-apply/utils/constants.ts
@@ -1,5 +1,5 @@
 export const cvUrls: Record<string, string> = {
-  frontend: 'https://drive.google.com/file/d/1RV1u8EX74Z-asYbgWHKvY1eh8pXcK8du/view?usp=drive_link',
+  frontend: 'https://drive.google.com/file/d/1FkX8Xx0JZQBNNm007KGN3I0gt3MI2ffy/view?usp=drive_link',
   backend:
     'https://drive.google.com/file/d/1RV1u8EX74Z-asYbgWHKvY1eh88373pXcK8du/view?usp=drive_link',
   fullstack:
